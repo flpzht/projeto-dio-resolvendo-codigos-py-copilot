48ec2e3fc5d75b30090c062add11f4b7a46122d2
Felipe <flpcsb@hotmail.com>
2025-11-30 19:57:16 -0300

    Add palindrome checking functionality
    Implementa verificação de palíndromos com entrada do usuário.

---
+++ resolucoes_code/palindromos.py
@@ -0,0 +1,11 @@
+# 6 - Verificando Palíndromos 🔄
+palavra = input("Digite uma palavra: ")
+
+# Invertendo a string
+if palavra == palavra[::-1]:
+    print("É um palíndromo!")
+else:
+    print("Não é um palíndromo.")
+
+
+# 👉 Aqui exploramos fatiamento de strings e o conceito de palíndromo.
