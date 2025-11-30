7aa2db10a9130322fca088118934edd262382666
Felipe <flpcsb@hotmail.com>
2025-11-30 19:56:46 -0300

    Add media_note.py to calculate average grades

---
+++ resolucoes_code/media_note.py
@@ -0,0 +1,10 @@
+# 5 - Calculando Média de Notas 📚
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print("A média das notas é:", media)
+
+
+# 👉 Uso de variáveis e aritmética para calcular médias.
